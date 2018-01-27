83d234739c0dc9069d56c500904f6eebceee5d65
lizhizhou <lzz@meteroi.com>
2018-01-27 11:32:46 +0800

    update vertex support

--- src/main/scala/nlp.scala
+++ src/main/scala/nlp.scala
@@ -1,43 +1,41 @@
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions._
 import com.databricks.spark.corenlp.functions._
+import com.arangodb.spark.ArangoSpark
 
 object NLP {
   def main(args: Array[String]) {
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val sc = spark.sparkContext
-    val data = Array(1, 2, 3, 4, 5)
-    val distData = sc.parallelize(data)
-    distData.collect.foreach(println)
-
     
+    val conf = new SparkConf()
+    .set("arangodb.host", "127.0.0.1")
+    .set("arangodb.port", "8529")
+    .set("arangodb.user", "root")
+    .set("arangodb.password", "lab123")
+    val spark = SparkSession.builder.appName("Simple Application").config(conf).getOrCreate()
+    val sc = spark.sparkContext
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)  
     import sqlContext.implicits._
     
     val input = Seq(
       (1, "<xml>Stanford University is located in California. It is a great university.</xml>")).toDF("id", "text")
-
-    val output = input
-      .select(cleanxml('text).as('doc))
-      .select(explode(ssplit('doc)).as('sen))
-      .select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), sentiment('sen).as('sentiment))
-
-    output.show(truncate = false)
+//
+//    val output = input
+//      .select(cleanxml('text).as('doc))
+//      .select(explode(ssplit('doc)).as('sen))
+//      .select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), sentiment('sen).as('sentiment))
+//
+//    output.show(truncate = false)
+      
+    val triple = sqlContext.read.json("/home/bigdata/microeco.json")  
+    val vertex = triple.select("object").union(triple.select("subject")).distinct()
+    //triple.show(200)
+    vertex.show(200)
+    // val rdd = ArangoSpark.load[MyBean](sc, "myCollection")
+    ArangoSpark.save(triple.rdd, "edge")
+    ArangoSpark.save(vertex, "vertex")
 
     sc.stop()
     
-    
-//val conf = new SparkConf()
-//    .set("arangodb.host", "127.0.0.1")
-//    .set("arangodb.port", "8529")
-//    .set("arangodb.user", "root")
-//    .set("arangodb.password", "lab123")
-//    ...
-//val sc = new SparkContext(conf)
-    
-//val rdd = ArangoSpark.load[MyBean](sc, "myCollection")
-//ArangoSpark.save(rdd, "myCollection")
-
   }
 }
