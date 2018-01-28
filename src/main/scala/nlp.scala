febd99c3ed93ceb81358adfa6910ac1e683de898
lizhizhou <lzz@meteroi.com>
2018-01-28 13:41:20 +0800

    Add grahx

--- src/main/scala/nlp.scala
+++ src/main/scala/nlp.scala
@@ -1,16 +1,17 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark._
 import org.apache.spark.graphx._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import com.databricks.spark.corenlp.functions._
 import org.elasticsearch.spark._
-import org.elasticsearch.spark.sql._  
+import org.elasticsearch.spark.sql._
 import com.arangodb.spark.ArangoSpark
 
 case class Concept(name: String)
-case class Triple(obj: String, rel:String, subject: String)   
+case class Triple(obj: String, rel: String, subject: String)
 
 object NLP {
   def main(args: Array[String]) {
@@ -28,31 +29,44 @@ object NLP {
 
     val input = Seq(
       (1, "<xml>Stanford University is located in California. It is a great university.</xml>")).toDF("id", "text")
-    
-        val output = input
-          .select(cleanxml('text).as('doc))
-          .select(explode(ssplit('doc)).as('sen))
-          .select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), sentiment('sen).as('sentiment))
-    
-        output.show(truncate = false)
+
+//    val output = input
+//      .select(cleanxml('text).as('doc))
+//      .select(explode(ssplit('doc)).as('sen))
+//      .select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), coref('sen).as('coref), openie('sen).as('openie), sentiment('sen).as('sentiment))
+//    output.show(truncate = false)
 
     val triple = sqlContext.read.json("/home/bigdata/microeco.json")
+    triple.show(10)    
 
-    // The schema is encoded in a string
-    val schemaString = ""
-    
     // Generate the schema based on the string of schema
     val schema = StructType(Array(StructField("Concept", StringType, nullable = true)))
     val vertex = spark.createDataFrame(triple.select("object").rdd.union(triple.select("subject").rdd).distinct(), schema)
-    //triple.show(200)
-    vertex.show(200)
+
+    vertex.show(10)
+
+    // Create an RDD for vertex
+    val concept: RDD[(VertexId, String)] = vertex.rdd.map(x => (x.hashCode(), x.getAs("Concept")))
+      
+    // Create an RDD for edges
+    val relationships: RDD[Edge[String]] = triple.rdd.map { x => Edge(x.getAs("object").hashCode(), x.getAs("subject").hashCode(), x.getAs("relation")) } 
+    
+    // Define a default user in case there are relationship with missing user
+    val defaultconcept = ""
+    // Build the initial Graph
+    val graph = Graph(concept, relationships, defaultconcept)
+    graph.vertices.collect().take(10).foreach(println)
+    graph.edges.collect().take(10).foreach(println)
+    
     // val rdd = ArangoSpark.load[MyBean](sc, "myCollection")
     // ArangoSpark.saveDF(triple, "edge")
     // ArangoSpark.saveDF(vertex, "vertex")
 
-    triple.saveToEs("spark/docs")
-    val es = sc.esRDD("spark/docs")
-    es.take(100).foreach(println)
+    triple.saveToEs("spark/vertex")
+    val es = sc.esRDD("spark/vertex")
+    es.take(10).foreach(println)
+    
+    
     sc.stop()
   }
 }
