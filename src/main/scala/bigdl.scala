e80a35e1885a919d67a0d89f7f65ef55de3656e5
lizhizhou <lzz@meteroi.com>
2018-03-06 10:18:33 +0800

    Add bigdl example

--- src/main/scala/bigdl.scala
+++ src/main/scala/bigdl.scala
@@ -1,7 +1,15 @@
 import java.nio.ByteOrder
-
 import com.intel.analytics.bigdl.nn._
 import com.intel.analytics.bigdl.numeric.NumericFloat
+import com.intel.analytics.bigdl.example.utils._
+import com.intel.analytics.bigdl.nn.{ClassNLLCriterion, _}
+import com.intel.analytics.bigdl.utils.{Engine, LoggerFilter, T}
+import org.apache.log4j.{Level => Levle4j, Logger => Logger4j}
+import org.slf4j.{Logger, LoggerFactory}
+import scopt.OptionParser
+
+import scala.collection.mutable.{ArrayBuffer, Map => MMap}
+import scala.language.existentials
 
 class bigdl {
 
@@ -19,4 +27,43 @@ object bigdl {
     val model = Module.loadTF(modelPath, inputs, outputs, ByteOrder.LITTLE_ENDIAN)
 
   }
+  
+object TextClassifier {
+  val log: Logger = LoggerFactory.getLogger(this.getClass)
+  LoggerFilter.redirectSparkInfoLogs()
+  Logger4j.getLogger("com.intel.analytics.bigdl.optim").setLevel(Levle4j.INFO)
+
+  def main(args: Array[String]): Unit = {
+    val localParser = new OptionParser[TextClassificationParams]("BigDL Example") {
+      opt[String]('b', "baseDir")
+        .required()
+        .text("Base dir containing the training and word2Vec data")
+        .action((x, c) => c.copy(baseDir = x))
+      opt[String]('p', "partitionNum")
+        .text("you may want to tune the partitionNum if run into spark mode")
+        .action((x, c) => c.copy(partitionNum = x.toInt))
+      opt[String]('s', "maxSequenceLength")
+        .text("maxSequenceLength")
+        .action((x, c) => c.copy(maxSequenceLength = x.toInt))
+      opt[String]('w', "maxWordsNum")
+        .text("maxWordsNum")
+        .action((x, c) => c.copy(maxWordsNum = x.toInt))
+      opt[String]('l', "trainingSplit")
+        .text("trainingSplit")
+        .action((x, c) => c.copy(trainingSplit = x.toDouble))
+      opt[String]('z', "batchSize")
+        .text("batchSize")
+        .action((x, c) => c.copy(batchSize = x.toInt))
+      opt[Int]('l', "learningRate")
+        .text("learningRate")
+        .action((x, c) => c.copy(learningRate = x))
+    }
+
+    localParser.parse(args, TextClassificationParams()).map { param =>
+      log.info(s"Current parameters: $param")
+      val textClassification = new TextClassifier(param)
+      textClassification.train()
+    }
+  }
+}
 }
