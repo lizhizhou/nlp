a6d34a5f496d84a68616c8aa9bea5219a09fe306
lizhizhou <lzz@meteroi.com>
2018-03-10 16:10:23 +0800

    Add UT for TripleGraphX

--- src/main/scala/TripleGraphX.scala
+++ src/main/scala/TripleGraphX.scala
@@ -38,8 +38,40 @@ object TripleGraphX {
     StructField("relation", StringType, nullable = true),
     StructField("subject", StringType, nullable = true)))
   def apply(spark: SparkSession) = new TripleGraphX(spark)
-  def unitTest()
+  def unitTest(spark: SparkSession)
   {
-    
+    val sc = spark.sparkContext
+    // Create an RDD for the vertices
+    val users: RDD[(VertexId, String)] =
+      sc.parallelize(Array(
+        (3L, "rxin"),
+        (7L, "jgonzal"),
+        (5L, "franklin"),
+        (2L, "istoica"),
+        // Following lines are new data
+        (8L, "bshears"),
+        (9L, "nphelge"),
+        (10L, "asmithee"),
+        (11L, "rmutt"),
+        (12L, "ntufnel")))
+    // Create an RDD for edges
+    val relationships: RDD[Edge[String]] =
+      sc.parallelize(Array(
+        Edge(3L, 7L, "collab"),
+        Edge(5L, 3L, "advisor"),
+        Edge(2L, 5L, "colleague"),
+        Edge(5L, 7L, "pi"),
+        // Following lines are new data
+        Edge(5L, 8L, "advisor"),
+        Edge(2L, 9L, "advisor"),
+        Edge(5L, 10L, "advisor"),
+        Edge(2L, 11L, "advisor")))
+    // Build the initial Graph
+    val graph = Graph(users, relationships)
+
+    val tg = TripleGraphX(spark)
+    val tf = tg.toTriple(graph)
+    val gf = tg.toGraphX(tf)
+
   }
 }
