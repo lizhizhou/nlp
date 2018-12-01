ef1afa4e3f35fd362ca567cf187f507870896adf
Zhizhou Li <lzz@meteroi.com>
2018-12-01 15:38:18 +0800

    Add ut for jieba
    Add ut for jieba

--- src/main/scala/nlp.scala
+++ src/main/scala/nlp.scala
@@ -142,7 +142,20 @@ object NLP {
     //  .option("rootTag", "books")
     //  .option("rowTag", "book")
     //  .xml("newbooks.xml")
+    
+    // import com.huaban.analysis.jieba.JiebaSegmenter
+    // import com.huaban.analysis.jieba.JiebaSegmenter.SegMode
+    // import scala.collection.JavaConversions._
+    // def jieba(s:String) =  {
+    //     val segmenter = new JiebaSegmenter()
+    //     segmenter.process(s, SegMode.SEARCH).toList.map(_.word + " ").foldLeft("")(_+_).toString
+    // }
 
+    // val sentences: Array[String] = Array[String]("这是一个伸手不见五指的黑夜。我叫孙悟空，我爱北京，我爱Python和C++。", "我不喜欢日本和服。", "雷猴回归人间。", "工信处女干事每月经过下属科室都要亲口交代24口交换机等技术性器件的安装工作", "结果婚的和尚未结过婚的")
+    // for (sentence <- sentences) {
+    //   println(
+    //       jieba(sentence))
+    // }
     sc.stop()
   }
 }
