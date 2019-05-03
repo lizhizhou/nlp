2fe3a2e8c87b22d5d926292189ad657bec61fc8f
lizhizhou <lzz@meteroi.com>
2019-05-03 17:46:54 +0800

    Add santi link

--- modeldownload.sh
+++ modeldownload.sh
@@ -1,3 +1,5 @@
 wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.9.1/stanford-corenlp-3.9.1-models.jar
 wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.9.1/stanford-corenlp-3.9.1-models-chinese.jar
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/word-vectors-v2/cc.zh.300.bin.gz
+wget http://www.zzs5.com/uploadfile/2016/1011/20161011035857541.txt
+dos2unix 20161011035857541.txt
