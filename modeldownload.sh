62576741d476b40d245cb2d3f3ad0e7ed21b9b23
Zhizhou Li <lzz@meteroi.com>
2019-05-27 14:42:01 +0800

    Fix Bug
    Fix Bug

--- modeldownload.sh
+++ modeldownload.sh
@@ -2,6 +2,6 @@ wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.9.1/stanf
 wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.9.1/stanford-corenlp-3.9.1-models-chinese.jar
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/word-vectors-v2/cc.zh.300.bin.gz
 wget https://ai.tencent.com/ailab/nlp/data/Tencent_AILab_ChineseEmbedding.tar.gz
-wget -O stopwords.txt https://github.com/goto456/stopwords/blob/master/%E4%B8%AD%E6%96%87%E5%81%9C%E7%94%A8%E8%AF%8D%E8%A1%A8.txt 
+wget -O stopwords.txt https://raw.githubusercontent.com/goto456/stopwords/master/%E4%B8%AD%E6%96%87%E5%81%9C%E7%94%A8%E8%AF%8D%E8%A1%A8.txt
 wget http://www.zzs5.com/uploadfile/2016/1011/20161011035857541.txt
 dos2unix 20161011035857541.txt
