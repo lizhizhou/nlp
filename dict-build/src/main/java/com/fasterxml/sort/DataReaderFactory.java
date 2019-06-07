0c3ef988cb10414e67602918f88027c413756767
lizhizhou1983 <lizhizhou1983@gmail.com>
2019-06-07 16:53:29 +0800

    cleanup and use git submodule

--- dict-build/src/main/java/com/fasterxml/sort/DataReaderFactory.java
+++
@@ -1,8 +0,0 @@
-package com.fasterxml.sort;
-
-import java.io.*;
-
-public abstract class DataReaderFactory<T>
-{
-    public abstract DataReader<T> constructReader(InputStream in) throws IOException;
-}
--- dict-build/src/main/java/com/fasterxml/sort/DataWriterFactory.java
+++
@@ -1,8 +0,0 @@
-package com.fasterxml.sort;
-
-import java.io.*;
-
-public abstract class DataWriterFactory<T>
-{
-    public abstract DataWriter<T> constructWriter(OutputStream out) throws IOException;
-}
