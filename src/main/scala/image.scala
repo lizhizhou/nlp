295e2b01f7aa340720b23d3031feb4f7b19c5837
lizhizhou <lzz@meteroi.com>
2020-03-16 21:17:47 +0800

    Add images file

---
+++ src/main/scala/image.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import com.intel.analytics.zoo.common.NNContext
+import com.intel.analytics.zoo.pipeline.nnframes.NNImageReader
+
+object image {
+  def main(args: Array[String]): Unit = {
+    val sc = NNContext.initNNContext("app")
+    val imageDF = NNImageReader.readImages("*.jpg", sc)
+    imageDF.show()
+  }
+}
