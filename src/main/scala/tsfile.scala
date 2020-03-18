fc48d641a2c95a6bb0bf778348529e1e9e47b3bc
lizhizhou1983 <lizhizhou1983@gmail.com>
2020-03-18 09:00:41 +0800

    Add tsfile support

--- src/main/scala/tsfile.scala
+++ src/main/scala/tsfile.scala
@@ -1,5 +1,14 @@
-package com.navercorp
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
 
-class tsfile {
-
+object tsfile {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    import spark.implicits._
+    val input = (1 to 3000).map(x => (x, Math.sin(x * 0.1)))
+      .toDF("time", "value")
+    input.show()
+    input.write.format("org.apache.iotdb.spark.tsfile").save("output")
+    spark.stop()
+  }
 }
