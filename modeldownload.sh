88507b388e545ca6dd1a9926ab3286658b986bc7
lizhizhou1983 <lizhizhou1983@gmail.com>
2020-04-20 09:52:17 +0800

    Add RDF Sample

--- modeldownload.sh
+++ modeldownload.sh
@@ -3,5 +3,6 @@ wget http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.9.1/stanf
 wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.zh.300.bin.gz
 wget https://ai.tencent.com/ailab/nlp/data/Tencent_AILab_ChineseEmbedding.tar.gz
 wget -O stopwords.txt https://raw.githubusercontent.com/goto456/stopwords/master/%E4%B8%AD%E6%96%87%E5%81%9C%E7%94%A8%E8%AF%8D%E8%A1%A8.txt
+wget https://jena.apache.org/tutorials/sparql_data/vc-db-1.rdf
 wget http://www.zzs5.com/uploadfile/2016/1011/20161011035857541.txt
 dos2unix 20161011035857541.txt
