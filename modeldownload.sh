bcd0c8360023d9d45438e53cb8c57c4b53c151a3
lizhizhou1983 <lizhizhou1983@gmail.com>
2020-05-10 15:36:44 +0800

    add download

--- modeldownload.sh
+++ modeldownload.sh
@@ -5,4 +5,5 @@ wget https://ai.tencent.com/ailab/nlp/data/Tencent_AILab_ChineseEmbedding.tar.gz
 wget -O stopwords.txt https://raw.githubusercontent.com/goto456/stopwords/master/%E4%B8%AD%E6%96%87%E5%81%9C%E7%94%A8%E8%AF%8D%E8%A1%A8.txt
 wget https://jena.apache.org/tutorials/sparql_data/vc-db-1.rdf
 wget http://www.zzs5.com/uploadfile/2016/1011/20161011035857541.txt
+wget http://lizhizhou.github.io/javascript.jpeg
 dos2unix 20161011035857541.txt
